68eddd97e16214723a4fddd4f83764a314bf085e
danilovalerio <danilo_vs@hotmail.com>
2024-03-22 12:03:03 -0300

    Igualdade com diversos tipos de dados

--- cypress/e2e/describe.cy.js
+++ cypress/e2e/describe.cy.js
@@ -1,13 +1,13 @@
-it.only("Meu teste externo", () => {});
+it("Meu teste externo", () => {});
 
 describe("Grupo de testes...", () => {
   describe("Grupo de testes mais específicos...", () => {
-    it.skip("Um teste específico...", () => {});
+    it("Um teste específico...", () => {});
   });
 
   describe("Grupo de testes mais específicos...", () => {
     it("Segundo teste específico...", () => {});
   });
 
-  it.only("Meu útlimo teste interno", () => {});
+  it("Meu útlimo teste interno", () => {});
 });
