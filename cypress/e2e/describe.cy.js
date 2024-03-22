5be93f9254c763f5e4a5b4413f4a6281da5470de
danilovalerio <danilo_vs@hotmail.com>
2024-03-22 08:46:00 -0300

    describe e it

---
+++ cypress/e2e/describe.cy.js
@@ -0,0 +1,13 @@
+it("Meu teste externo", () => {});
+
+describe("Grupo de testes...", () => {
+  describe("Grupo de testes mais específicos...", () => {
+    it("Um teste específico...", () => {});
+  });
+
+  describe("Grupo de testes mais específicos...", () => {
+    it("Segundo teste específico...", () => {});
+  });
+
+  it("Meu teste interno", () => {});
+});
